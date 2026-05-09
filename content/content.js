332b9a3e9fa6fb0cba555ca6a631424f7d277be5
Wonho Bae <77373106+lyh4215@users.noreply.github.com>
2026-05-09 01:34:22 +0000

    equation dom

--- content/content.js
+++ content/content.js
@@ -49,7 +49,7 @@ const TABLE_TEXT_FONT_SIZE_PT = 10.5;
 const TABLE_TOP_GAP_PT = 5.0;
 
 const EQUATION_DISPLAY_MARGIN_TOP_PT = 7;
-const EQUATION_DISPLAY_MARGIN_BOTTOM_PT = 9;
+const EQUATION_DISPLAY_MARGIN_BOTTOM_PT = 11;
 
 // 연속 equation 사이의 최소 visual gap 참고값.
 // 실제 pushEquationBlock에서는 top gap 중복을 제거한다.
